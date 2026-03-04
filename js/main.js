a8a9b3b9195e96b9e4f2d07377c89437c760c820
lilitantonyan <lilit@helix.am>
2026-03-04 20:55:50 +0400

    add responsive style

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ var swiper = new Swiper(".review__list", {
         prevEl: ".swiper-button-prev",
     },
 });
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.querySelectorAll('.video').forEach(wrapper => {
+        console.log('aaa')
+        wrapper.addEventListener('click', function() {
+            const videoSrc = this.dataset.video;
+            this.innerHTML = `
+      <video 
+        src="${videoSrc}" 
+        controls 
+        autoplay 
+       >
+      </video>
+    `;
+
+        });
+    });
+});
+
